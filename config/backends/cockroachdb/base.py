e62dad82039010292e6a4addb7bbe42fcd2eef6d
amansingh2116 <msingh9968429505@gmail.com>
2026-08-10 05:00:03 +0530

    fix: disable varchar_pattern_ops operator classes in CockroachDB backend

--- config/backends/cockroachdb/base.py
+++ config/backends/cockroachdb/base.py
@@ -7,8 +7,10 @@ CockroachDB is PostgreSQL wire-compatible but has a few key differences:
 1. Version check: reports 'PostgreSQL 13.0' which Django 5.x rejects by default.
 2. Constraints: does NOT support DEFERRABLE / INITIALLY DEFERRED constraints.
 3. Column inline FK: does NOT support 'SET CONSTRAINTS ... IMMEDIATE'.
+4. Index operator classes: does NOT support varchar_pattern_ops/text_pattern_ops on B-tree indexes.
 """
 
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 from django.db.backends.postgresql.base import DatabaseWrapper as _PgWrapper
 from django.db.backends.postgresql.features import DatabaseFeatures as _PgFeatures
 from django.db.backends.postgresql.operations import DatabaseOperations as _PgOperations
@@ -31,6 +33,19 @@ class DatabaseSchemaEditor(_PgSchemaEditor):
     sql_create_column_inline_fk = (
         "CONSTRAINT %(name)s REFERENCES %(to_table)s(%(to_column)s)"
     )
+
+    def _create_like_index_sql(self, model, field):
+        """
+        CockroachDB does not support varchar_pattern_ops / text_pattern_ops
+        operator classes on standard B-tree indexes.
+        """
+        return None
+
+    def _index_columns(self, table, columns, col_suffixes, opclasses):
+        """
+        Strip operator classes since CockroachDB only allows them for inverted/vector indexes.
+        """
+        return BaseDatabaseSchemaEditor._index_columns(self, table, columns, col_suffixes, ())
 
 
 class DatabaseWrapper(_PgWrapper):
